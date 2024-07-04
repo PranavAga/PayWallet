73f9ac619c14abfb32cb733eda2cbce1809ddab0
Pranav Agarwal <94969289+PranavAga@users.noreply.github.com>
2024-07-04 22:58:24 +0530

    initialized

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      mechant-app
+      <Button appName="mechant-app">Click</Button>
+    </div>
+  );
+}
